9f8faf10ce03dbdd5563a533f05764345a4b0ba8
PraveenKumar-Kasoju <60596414+PraveenKumar-Kasoju@users.noreply.github.com>
2024-06-12 09:47:25 +0530

    Update example_lambda_function.py

--- example_lambda_function.py
+++ example_lambda_function.py
@@ -1,7 +1,6 @@
 import json
 
 def lambda_handler(event, context):
-    # Your primary Lambda function logic
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
