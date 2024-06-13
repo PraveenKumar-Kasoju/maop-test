f97bbbe30869e8e0c1a9a89d639067cef7cd54a3
PraveenKumar-Kasoju <60596414+PraveenKumar-Kasoju@users.noreply.github.com>
2024-06-13 16:25:05 +0530

    Update hello-python.py

--- terraform/hello-python.py
+++ terraform/hello-python.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
-   message = 'Hello World {} !'.format(event['key1'])
+   message = 'Hello World, I am Praveen {} !'.format(event['key1'])
    return {
        'message' : message
    }
